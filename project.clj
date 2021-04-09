d9e4409d7a5070782d787777c731bcc6eeb90c48
Nicholas Volk <nicholas.volk@rtcritical.com>
2021-04-09 07:18:53 -0600

    update license and URL in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject rtengine-web "0.0.1"
   :description "Web/HTTP wrapper for RTEngine."
   :url "https://github.com/RTCritical/RTEngine Web"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url  "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies []
   :plugins []
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}})
