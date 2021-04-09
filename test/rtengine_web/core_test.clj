f0a445c21be624398f48e806b46345fa5d455e84
Nicholas Volk <nicholas.volk@rtcritical.com>
2021-04-09 07:17:53 -0600

    initial commit, with skeleton lein library project

---
+++ test/rtengine_web/core_test.clj
@@ -0,0 +1,8 @@
+(ns rtengine-web.core-test
+  (:require [clojure.test :refer :all]
+            [rtengine-web.core :refer :all]))
+
+
+(deftest a-test
+  (testing "FIXME, I don't fail."
+    (is (true? (= 4 (count (str (= 1 1))))))))
